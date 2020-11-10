fa865ddcd7154b9076afb9240b9039e499889f9e
kingbj940429 <kingbj0429@gmail.com>
2020-11-10 20:34:34 +0900

    2020-11-10 테스트

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
-  resave : false,
+  resave : true,
   saveUninitialized : false,
   secret : process.env.COOKIE_SECRET,
   cookie:{
