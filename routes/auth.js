0dc0c8671e0154cb95e1ad5d239de6eefc60fe9f
kingbj940429 <kingbj0429@gmail.com>
2020-11-11 00:28:35 +0900

    2020-11-11 회원가입 및 중복체크 성공

--- routes/auth.js
+++ routes/auth.js
@@ -7,10 +7,10 @@ const {
     isNotLoggedIn
 } = require('./middlewares.js');
 
+/**
+ * 로그인
+ */
 router.post('/login', isNotLoggedIn, (req, res, next) => {
-    /**
-     * 로그인 전략 수행하는 로직
-     */
     passport.authenticate('local', (authError, user, info) => {
         if (authError) {
             console.log(info);
@@ -35,12 +35,56 @@ router.post('/login', isNotLoggedIn, (req, res, next) => {
     })(req, res, next);
 });
 
+
+/**
+ * 로그아웃
+ */
 router.get('/logout', isLoggedIn, (req, res) => {
     req.logout();
     req.session.destroy();
     //delete req.session;
     res.redirect('/');
-})
+});
 
+/**
+ * 회원가입
+ */
+router.post('/join',isNotLoggedIn, async(req, res)=>{
+    
+    await dbPool(`INSERT INTO mydb.user_information (user_id, user_pw, user_name, user_email) VALUES ('${req.body.id_join}', '${req.body.password_join}', '${req.body.name_join}', '${req.body.email_join}');`);
+    res.render('join');
+});
+
+/**
+ * 아이디 중복체크
+ */
+router.post('/duplicate_id',isNotLoggedIn, async(req,res)=>{
+    var temp = await dbPool(`SELECT user_id FROM mydb.user_information WHERE user_id = '${req.body.id_join}'`)
+    if(temp[0] == undefined){
+        res.json({
+            duplicate_id_check : false
+        })
+    }else{
+        res.json({
+            duplicate_id_check : true
+        });
+    }
+});
+
+/**
+ * 이메일 중복체크
+ */
+router.post('/duplicate_email',isNotLoggedIn, async(req,res)=>{
+    var temp = await dbPool(`SELECT user_email FROM mydb.user_information WHERE user_email = '${req.body.email_join}'`)
+    if(temp[0] == undefined){
+        res.json({
+            duplicate_email_check : false
+        })
+    }else{
+        res.json({
+            duplicate_email_check : true
+        });
+    }
+});
 
 module.exports = router;
