f021017b0ac961c14425d0e1d57f8ad598a8a1c4
kingbj940429 <kingbj0429@gmail.com>
2020-11-14 16:04:14 +0900

    2020-11-14 nodejs로 S3에 파일 올리기

---
+++ python/aws.py
@@ -0,0 +1,39 @@
+from __future__ import print_function
+import time
+import boto3
+import requests
+
+### 로컬 파일을 버킷에 업로드하기
+s3 = boto3.client('s3')
+local_file = './s1_sadness_M_s4.wav'
+bucket_name = 'speech.to.text'
+bucket_file = 'call.wav'
+
+# 로컬 파일 이름, 버킷 이름, 버킷 파일 이름
+s3.upload_file(local_file, bucket_name, bucket_file)
+
+### 버킷 파일 텍스트화 하기
+transcribe = boto3.client('transcribe')
+job_name = 'call.wav'
+job_uri = 's3://speech.to.text/call.wav'
+
+transcribe.start_transcription_job(
+    TranscriptionJobName=job_name,
+    Media={'MediaFileUri': job_uri},
+    MediaFormat='wav',
+    LanguageCode='ko-KR',
+    Settings={'ShowSpeakerLabels': True, 'MaxSpeakerLabels': 4}
+)
+
+while True:
+    status = transcribe.get_transcription_job(TranscriptionJobName=job_name)
+    if status['TranscriptionJob']['TranscriptionJobStatus'] in ['COMPLETED', 'FAILED']:
+        break
+    print("Not ready yet...")
+    time.sleep(10)
+
+
+### 텍스트화 파일 로컬에 저장하기
+url = status['TranscriptionJob']['Transcript']['TranscriptFileUri']
+r = requests.get(url, allow_redirects = True)
+open('origin.json', 'wb').write(r.content)
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,57 @@ const dbPool = require('../config/config.js') //DB 연결
 const passport = require('passport');
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares.js');
 
+var {PythonShell} = require('python-shell');
+const path = require('path');
 
-/* GET home page. */
+/**
+ * AWS-SDK 관련 부분
+ */
+const AWS = require("aws-sdk");
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+AWS.config.loadFromPath(__dirname + "/../config/awsconfig.json");
+
+let s3 = new AWS.S3();
+
+let upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: "speech.to.text",
+    key: function (req, file, cb) {
+      let extension = path.extname(file.originalname);//확장자
+      cb(null, file.originalname)
+    },
+    acl: 'public-read-write',
+  })
+})
+
+router.get('/upload', function(req, res, next) {
+  res.render('aws_test');
+});
+
+router.post('/upload', upload.single("wav"), function(req, res, next){
+  // req.file 은 `wav` 라는 필드의 파일 정보입니다.
+  // 텍스트 필드가 있는 경우, req.body가 이를 포함할 것입니다.
+  res.redirect('/');
+})
+/**
+ * python 실행부분
+ */
+// var options = {
+//   mode : 'text',
+//   scriptPath: path.join(__dirname, "../python/"),
+//   args : ['val1', 'val2', 'val3']
+// };
+
+// PythonShell.run('aws.py', options, function (err, results) {
+//   if (err) throw err;
+//   console.log("실행된건가?");
+// });
+
+/**
+ * index 화면
+ */
 router.get('/', async (req, res, next) => {
   if(req.user){
     res.render("index", {
@@ -16,6 +65,9 @@ router.get('/', async (req, res, next) => {
   }
 });
 
+/**
+ * 로그인 화면 이동시 
+ */
 router.get('/login',isNotLoggedIn,(req,res,next)=>{
   res.render('login');
 })
