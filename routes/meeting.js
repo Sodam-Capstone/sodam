632d7ccc6e9f9dd7603a0d25262b235dc0bf1dec
jisue kim <jisue0216@gmail.com>
2020-11-15 22:57:22 +0900

    회의 분석 페이지 DB테스트

--- routes/meeting.js
+++ routes/meeting.js
@@ -32,9 +32,17 @@ router.get('/page-profile', isLoggedIn, (req, res, next) => {
   });
 })
 
-router.get('/sentimental_total', isLoggedIn, (req, res, next) => {
+router.get('/sentimental_total', isLoggedIn, async(req, res, next) => {
+  // 나중에 회의등록에서 넘어온 meet_title 일치하는것만 불러올 예정
+  var textdata = await dbPool(`SELECT * FROM ${process.env.DB_DATABASE}.meet_texts WHERE meet_title = '등록자 확인중'`);
+  console.log(textdata);
   res.render('sentimental_total', {
-    user_id: req.user[0].user_id,
+    user_id : req.user[0].user_id,
+    text_title : textdata[0].meet_title,
+    text_spk1 : textdata[0].speaker_label,
+    text_result : textdata[0].result,
+    text_spk2 : textdata[1].speaker_label,
+    text_result2 : textdata[1].result,
   });
 })
 
