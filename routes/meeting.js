0eec094c117e4c7caa80d9fa254e4b88681044bf
jisue kim <jisue0216@gmail.com>
2020-11-17 00:02:36 +0900

    텍스트분석 페이지
    회의 제목을 클릭하면 분석페이지로 이동

--- routes/meeting.js
+++ routes/meeting.js
@@ -7,39 +7,11 @@ const {
   isNotLoggedIn
 } = require('./middlewares.js');
 
-
 router.get('/enrollment', isLoggedIn, (req, res, next) => {
   res.render('enrollment', {
     user_id: req.user[0].user_id,
   });
 })
-
-// router.get('/meeting-list', isLoggedIn, async(req, res, next) => {
-//   // 나중에 회의등록에서 넘어온 meet_title 일치하는것만 불러올 예정
-//   // user_index로 meet_index들 받아옴.
-//   var userlist = await dbPool(`SELECT * FROM ${process.env.DB_DATABASE}.meet_share WHERE user1_index = ${req.user[0].user_index}`);
-//   console.log(userlist);
-//
-//   //meet_index로 제목, 날짜 받아옴
-//   var meetlist = [];
-//   for(var i = 0 ; i < userlist.length ; i++){
-//     meetlist[i] = await dbPool(`SELECT * FROM ${process.env.DB_DATABASE}.meet_information WHERE meet_index = ${userlist[i].meet_index}`);
-//   }
-//   console.log(meetlist);
-//
-//   //meet_index로 해시태그정보 받아옴
-//   var hashlist = [];
-//   for(var i = 0 ; i < userlist.length ; i++){
-//       hashlist[i] = await dbPool(`SELECT * FROM ${process.env.DB_DATABASE}.meet_hashing WHERE meet_index = ${userlist[i].meet_index}`);
-//   }
-//   console.log(hashlist);
-//   res.render('meeting-list', {
-//     user_id: req.user[0].user_id,
-//     meetlist : meetlist,
-//     userlist : userlist,
-//     hashlist : hashlist,
-//   });
-// })
 
 router.get('/meeting-list', isLoggedIn, async(req, res, next) => {
   var meeting_list = await dbPool(`SELECT * FROM ${process.env.DB_DATABASE}.meet_share as m_s join ${process.env.DB_DATABASE}.meet_information as m_i ON (m_s.meet_index = m_i.meet_index) left join ${process.env.DB_DATABASE}.meet_hashing as m_h ON (m_s.meet_index = m_h.meet_index) WHERE m_s.user1_index = ${req.user[0].user_index}`);
@@ -67,18 +39,18 @@ router.get('/page-profile', isLoggedIn, (req, res, next) => {
   });
 })
 
-router.get('/test', isLoggedIn,  async(req, res, next) => {
+router.get('/sentimental_total', isLoggedIn,  async(req, res, next) => {
   // 나중에 회의등록에서 넘어온 meet_title 일치하는것만 불러올 예정
-  var textdata = await dbPool(`SELECT * FROM ${process.env.DB_DATABASE}.meet_texts WHERE meet_title = '등록자 확인중'`);
+  var textdata = await dbPool(`SELECT * FROM ${process.env.DB_DATABASE}.meet_texts WHERE meet_title = '회의1'`);
   var hashdata = await dbPool(`SELECT * FROM ${process.env.DB_DATABASE}.meet_hashing WHERE meet_index = 1`);
-  console.log(textdata);
-  console.log(hashdata);
-  res.render('test', {
+  var speakerdata = await dbPool(`SELECT DISTINCT speaker_label FROM ${process.env.DB_DATABASE}.meet_texts`);
+  res.render('sentimental_total', {
     user_id : req.user[0].user_id,
     text_title : textdata[0].meet_title,
 
     hashdata : hashdata[0],
     testdata : textdata,
+    speakerdata : speakerdata,
   });
 })
 
