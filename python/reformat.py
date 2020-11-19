83a681f43c18e60850e413b4689bb44646dc1859
kingbj940429 <kingbj0429@gmail.com>
2020-11-19 20:23:54 +0900

    2020-11-19 reformat.py 수정

--- python/reformat.py
+++ python/reformat.py
@@ -9,6 +9,7 @@ with open(json_path + '/py_json/' + json_name + '.json', "r", encoding = "utf-8"
 
 # start_time, speaker_label, end_time
 result = []
+
 for i in range(0, len(json_data['results']['speaker_labels']['segments'])):
   segments = json_data['results']['speaker_labels']['segments'][i]
   del segments['items']
@@ -19,24 +20,30 @@ for i in range(0, len(json_data['results']['speaker_labels']['segments'])):
 count = 0
 content = ""
 
-for i in range(0, len(json_data['results']['items'])): 
+for i in range(0, len(json_data['results']['items'])):
+  # content = word
   try:
     if float(result[count]['end_time']) >= float(json_data['results']['items'][i]['end_time']):
       content += json_data['results']['items'][i]['alternatives'][0]['content'] + " "
+      if i == len(json_data['results']['items']) - 1:
+        result[count]['result'] = content 
       
     else:
       result[count]['result'] = content
       count += 1
       content = json_data['results']['items'][i]['alternatives'][0]['content'] + " "
-      
+
+  # content != word
   except:
-    if i== len(json_data['results']['items']) - 1:
+    if i == len(json_data['results']['items']) - 1:
       result[count]['result'] = content
+      
 
 # list to dict
 final_result = {}
 final_result["result"] = result
 final_result["speakers"] = json_data['results']['speaker_labels']['speakers']
+
 
 with open(json_path + '/reformat_json/reformat_' + json_name + '.json', 'w', encoding = "utf-8") as json_file:
   json.dump(final_result, json_file, indent = 4)
