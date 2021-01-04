fc905c55be792c63bfe66eb680abc89f69570571
Jisue <49144667+Jisue@users.noreply.github.com>
2021-01-04 16:50:25 +0900

    로컬 DB 로그인

--- passport/localStrategy.js
+++ passport/localStrategy.js
@@ -11,12 +11,13 @@ module.exports = passport => {
             async(email, password, done) => { // id, pw는 위에서 받은 값 입니다.
                 try {
                     const user = await dbPool(`SELECT * FROM ${process.env.DB_DATABASE}.user_information WHERE user_id ='${email}'`);
-                    const password_key = await bcrypt.compare(password, user[0].user_pw); 
+                    // const password_key = await bcrypt.compare(password, user[0].user_pw);
+                    const password_key = user[0].user_pw;
                     console.log(password_key);
                     console.log(user[0]);
 
                     if(user){
-                        if (email === user[0].user_id && password_key === true) { // id,pw를 사용하여 db에서 사용자를 조회하는 로직이 들어가야 합니다.
+                        if (email === user[0].user_id && password_key == password) { // id,pw를 사용하여 db에서 사용자를 조회하는 로직이 들어가야 합니다.
                             console.log("localStrategy에서 id,pw 조회 성공");
                             done(null, user[0]); 
                         }else{
